4b450141ad8f6be3aee2971a2204a412cec4dddd
Patil Rameshwar <rameshwarr.p001@gmail.com>
2026-01-26 23:47:20 +0530

    java start

---
+++ p1.java
@@ -0,0 +1,26 @@
+// class p1{
+//     public static void main(String args[]){
+//         System.out.println("Hello world");
+//     }
+// }
+
+// class p1{
+//     public static void main(String args[]){
+//         System.out.println("*");
+//         System.out.println("**");
+//         System.out.println("***");
+//         System.out.println("****");
+//         System.out.println("*****");
+    
+//     }
+// }
+
+
+class p1{
+    public static void main(String args[]){
+        int a =10;
+        int b=20;
+        System.out.println(2*(a+b));
+    
+    }
+}
