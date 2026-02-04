7e2d03a4fcb33b14fbfad05728a5e9ebf5315dd2
Patil Rameshwar <rameshwarr.p001@gmail.com>
2026-02-04 21:34:35 +0530

    day 3

--- p1.java
+++ p1.java
@@ -176,6 +176,7 @@
 //     }
 // }
 
+//// for loop 
 // import java.util.Scanner;
 // public class p1 {
 //     public static void main(String[] args) {
@@ -189,14 +190,50 @@
 //     }
 // }
 
-import java.util.Scanner;
-public class p1 {
-    public static void main(String[] args) {
-        Scanner sc =new Scanner(System.in);
-        int n=sc.nextInt();
-        for(int i=1;i<11;i++){
-            System.out.println(n*i);
-        }   
-    }
-}
+
+//// for loop 
+// import java.util.Scanner;
+// public class p1 {
+//     public static void main(String[] args) {
+//         Scanner sc =new Scanner(System.in);
+//         int n=sc.nextInt();
+//         for(int i=1;i<11;i++){
+//             System.out.println(n*i);
+//         }   
+//     }
+// }
+
+
+///// pattern 
+
+//// full rectangle 
+// public class p1 {
+
+//     public static void main(String[] args) {
+//         for(int i=0;i<4;i++){                                           ///// for row 
+//             for(int j=0;j<5;j++){                                      ///// for coloumn
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+// public class p1 {
+//     public static void main(String[] args) {
+//         for(int i=1;i<=4;i++){
+//             for(int j=1;j<=5;j++){
+//                 if (i==1 || j==1 || i==4 || j ==5 ) {
+//                     System.out.print('*');
+//                 }
+//                 else{
+//                     System.out.print(" ");
+//                 }
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
 
