b2c9d2be87547d79685f0183a179601e20ae1e4e
Patil Rameshwar <rameshwarr.p001@gmail.com>
2026-02-06 22:26:42 +0530

    day 4

--- p1.java
+++ p1.java
@@ -237,6 +237,43 @@
 // }
 
 
+//// paramid
+// public class p1 {
+//     public static void main(String[] args) {
+//         for(int i=1;i<=4;i++){
+//             for(int j=0;j<i;j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
 
 
+////reverse paramid 
+// public class p1 {
+//     public static void main(String[] args) {
+//         for(int i=5;i>=1;i--){
+//             for(int j=1;j<i;j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
 
+
+//// mirror paramid
+public class p1 {
+    public static void main(String[] args) {
+        for(int i=1;i<=4;i++){
+            for(int j=1;j<=4-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
